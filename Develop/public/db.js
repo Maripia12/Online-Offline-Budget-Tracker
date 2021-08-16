400c4f53de189e135c23221442cfa444d5154312
Pia Paulino <maripia1220@gmail.com>
2021-08-15 22:58:14 -0400

    Updated db.js

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -1,22 +1,16 @@
 let db;
 
-const request = indexedDB.open('budget' , 1);
+const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("BudgetList", { autoIncrement: true });
+};
 
-    const db = event.target.result;
-    db.createObjectStore('BudgetList', { autoIncrement: true });
-    };
+request.onsuccess = (event) => {
+  db = event.request.result;
+};
 
-    
-    request.onsuccess = event => {
-        db = event.request.result;
-
-    }
-
-
-
-    request.onerror = function (event) {
-      console.log(`Woops! ${event.target.errorCode}`);
-    };
-
+request.onerror = function (event) {
+  console.log(`Woops! ${event.target.errorCode}`);
+};
